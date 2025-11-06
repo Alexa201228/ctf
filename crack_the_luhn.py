fb43df5e472876f3a7f1b1fd613e50a9530aa570
alexandra_gridneva <aleksandra.gridneva@eqvilent.com>
2025-11-06 16:21:41 +0300

    Cracked card number challenge

--- crack_the_luhn.py
+++ crack_the_luhn.py
@@ -1,59 +1,23 @@
 
-
 def get_card_number(broken_card_num: str) -> str:
-    symbols_list = list(broken_card_num)
-    stars_idx = [i for i in range(len(symbols_list)) if symbols_list[i] == "*"]
-    mask = [2, 1] * 8
-    mask_nums = [mask[i] for i in range(len(mask)) if i in stars_idx]
-    symbols_list = [int(s) for s in symbols_list if s != "*"]
-    digits_sum = sum(symbols_list)
-    num = 1
-    pos = 0
-    prev_num = 0
-    while digits_sum % 10:
-        temp_sum = digits_sum
+    start_num = int(broken_card_num.replace("*", "0"))
+    end_num = int(broken_card_num.replace("*", "9"))
 
-        curr_arr = []
-        for i in range(len(mask_nums)):
-            
-            if i == pos:
-                curr_num = mask_nums[i] * num
-                print(curr_num)
-                while curr_num > 9:
-                    curr_num = curr_num // 10 + curr_num % 10
-                
-                temp_sum += curr_num
-                curr_arr.append(num)
-            else:
-                curr_num = mask_nums[i] * prev_num
-                while curr_num > 9:
-                    curr_num = curr_num // 10 + curr_num % 10
-                temp_sum += curr_num
-                curr_arr.append(prev_num)
-            
-            if temp_sum % 10 == 0:
-                digits_sum = temp_sum
-                print(curr_arr)
-                break
-        print(curr_arr)
-        if digits_sum % 10 == 0:
+    mask = [2, 1] * 8
+    for i in range(start_num, end_num + 10000, 10000):
+        digit_sum = 0
+        cur_num = list(str(i))
+        for j in range(len(cur_num)):
+            cur_sum = int(cur_num[j]) * mask[j]
+            while cur_sum > 9:
+                cur_sum = cur_sum // 10 + cur_sum % 10
+            digit_sum += cur_sum
+        
+        if digit_sum % 10 == 0 and i % 123457 == 0:
+            print(i)
             break
 
 
-
-
-        pos += 1
-        print(pos)
-        if pos == len(mask_nums):
-            pos = 0
-            prev_num = num
-            num += 1
-            
-
-
-
-
-
-print(get_card_number("543210******1234"))
+get_card_number("543210******1234")
 
 
